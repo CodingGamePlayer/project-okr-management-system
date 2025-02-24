0f6c644ffd8c8549c3761acd8ee4c02c8376379a
KimJongSik <otw1917@naver.com>
2025-02-25 08:29:04 +0900

    refactor: okr 테이블에 필요한 내용을 일부 변경

--- src/common/entities/OKRKeyResult.entity.ts
+++ src/common/entities/OKRKeyResult.entity.ts
@@ -1,10 +1,36 @@
-import { Entity, PrimaryColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
 import { OKR } from './OKR.entity';
+import { KeyResultUnit } from '../../domain/okr/dto/key-result-unit.enum';
 
 @Entity('okr_key_results')
 export class OKRKeyResult {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
   okr_id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column({
+    type: 'enum',
+    enum: KeyResultUnit,
+    default: KeyResultUnit.NUMBER,
+  })
+  unit: KeyResultUnit;
+
+  @Column('float')
+  target_value: number;
+
+  @Column('float')
+  current_value: number;
+
+  @Column('float')
+  weight: number;
 
   @Column('float')
   progress: number;
